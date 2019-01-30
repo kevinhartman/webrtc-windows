2e0e9f74079740d4507cb19632368f13e2e8fbef
Kevin Hartman <kevin@hart.mn>
2019-01-30 01:16:01 -0500

    s

--- third_party/winuwp_h264/H264Decoder/H264Decoder.cc
+++ third_party/winuwp_h264/H264Decoder/H264Decoder.cc
@@ -37,8 +37,12 @@ namespace webrtc {
 // H264 WinUWP Decoder Implementation
 //////////////////////////////////////////
 
+// TODO: use consistent return code handling convention (ON_SUCCEEDED for RTC)
+// TODO: use rtc C++ coding style (var naming etc)
 // TODO: it may be important that the same thread is used when interfacing with
 // Media Foundation transform decoder
+// TODO: add "needs key frame" member to discard until key frame.
+// TODO:check if auto-create output sample is available
 // TODO: set CODECAPI_AVLowLatencyMode in case Windows 8 (otherwise it fills all
 // buffers before emitting decoded frames resulting in huge latency
 // TODO: set varient_true to copy attributes from in to out samples:
@@ -58,11 +62,43 @@ WinUWPH264DecoderImpl::~WinUWPH264DecoderImpl() {
   Release();
 }
 
+HRESULT SupportsMediaType(ComPtr<IMFTransform> decoder, GUID media_type, bool* supported) {
+  *supported = false;
+
+  int type = 0;
+  while (true) {
+    ComPtr<IMFMediaType> spMediaType;
+    HRESULT hr = decoder->GetOutputAvailableType(0, type, &spMediaType);
+    if (hr == MF_E_NO_MORE_TYPES)
+      return S_OK;
+
+    GUID cur_type;
+    ON_SUCCEEDED(spMediaType->GetGUID(MF_MT_SUBTYPE, &cur_type));
+    if (FAILED(hr))
+      return hr;
+
+	if (cur_type == media_type) {
+      *supported = true;
+      return hr;
+    }
+
+    type++;
+  }
+}
+
 int WinUWPH264DecoderImpl::InitDecode(const VideoCodec* inst,
   int number_of_cores) {
   RTC_LOG(LS_INFO) << "WinUWPH264DecoderImpl::InitDecode()\n";
 
-  HRESULT hr = CoInitializeEx(0, COINIT_APARTMENTTHREADED); // TODO: uninitialize when finished
+  ULONG frameRateNumerator = 30; /* TODO: take framerate from inst: inst->maxFramerate ? */
+  ULONG frameRateDenominator = 1;
+  ULONG imageWidth = inst->width;
+  ULONG imageHeight = inst->height;
+
+  width_ = imageWidth;
+  height_ = imageHeight;
+
+  HRESULT hr = CoInitializeEx(0, COINIT_APARTMENTTHREADED);
   if (SUCCEEDED(hr))
     hr = MFStartup(MF_VERSION, 0);
 
@@ -71,15 +107,9 @@ int WinUWPH264DecoderImpl::InitDecode(const VideoCodec* inst,
       IID_IUnknown, (void**)&m_spDecoder);
 
   if (FAILED(hr))
-	  // TODO: log hr
     return WEBRTC_VIDEO_CODEC_ERROR;
 
   // Create input media type
-  ULONG frameRateNumerator = 30; /* TODO: take framerate from inst: inst->maxFramerate ? */
-  ULONG frameRateDenominator = 1;
-  ULONG imageWidth = inst->width;
-  ULONG imageHeight = inst->height;
-
   // TODO: these "SUCCEEDED" macros will all fall through if failure. Ensure this is expected paradigm.
   ComPtr<IMFMediaType> spInputMedia;
   hr = MFCreateMediaType(&spInputMedia);
@@ -100,26 +130,12 @@ int WinUWPH264DecoderImpl::InitDecode(const VideoCodec* inst,
   if (SUCCEEDED(hr))
     hr = m_spDecoder->SetInputType(0, spInputMedia.Get(), 0);
 
+  // Assert MF supports I420 output
+  bool i420_supported;
+  hr = SupportsMediaType(m_spDecoder, MFVideoFormat_I420, &i420_supported);
 
-  // TODO: here, we query the list of supported output formats after providing the input stream above
-  //       this is a temporary check. Remove this unless it's possible for h264 decoder impl to not support I420 on some devices.
-  //int type = 0;
-  //while (true) {
-  //  ComPtr<IMFMediaType> spMediaType;
-  //  hr = m_spDecoder->GetOutputAvailableType(0, type, &spMediaType);
-  //  if (hr == MF_E_NO_MORE_TYPES)
-  //    return E_FAIL;
-  //  GUID mediatype;
-  //  if (SUCCEEDED(hr)) {
-  //    hr = spMediaType->GetGUID(MF_MT_SUBTYPE, &mediatype);
-  //    if (SUCCEEDED(hr)) {
-  //      if (mediatype == MFVideoFormat_I420) {
-  //        break;
-  //      }
-  //    }
-  //  }
-  //  type++;
-  //}
+  if (FAILED(hr) || !i420_supported)
+    return WEBRTC_VIDEO_CODEC_ERROR;
 
   // Create output media type
   ComPtr<IMFMediaType> spOutputMedia;
@@ -164,6 +180,7 @@ int WinUWPH264DecoderImpl::InitDecode(const VideoCodec* inst,
   return WEBRTC_VIDEO_CODEC_ERROR;
 }
 
+// TODO: safe delete this class from project
 // Used to store an encoded H264 sample in a VideoFrame
 class H264NativeHandleBuffer : public NativeHandleBuffer {
 public:
@@ -183,13 +200,76 @@ private:
   ComPtr<IMFSample> _sample;
 };
 
-// TODO: comments for these params
+HRESULT CopyOutBuffer2D(uint8_t* pDestData,
+                        ComPtr<IMF2DBuffer> src,
+                        uint32_t width,
+                        uint32_t height) {
+  // Get raw pointer to image buffer
+  BYTE* pSrcData;
+  LONG stride;
+  HRESULT hr = src->Lock2D(&pSrcData, &stride);
+  if (FAILED(hr))
+    return hr;
+
+  // Copy data, ignoring pad.
+  for (DWORD row = 0; row < height; row++) {
+    memcpy(pDestData, pSrcData, width);
+    pDestData += width;
+    pSrcData += stride;
+  }
+
+  // We got our data. Let it go... let it go.
+  hr = src->Unlock2D();
+  return hr;
+}
+
+HRESULT CopyOutBuffer(uint8_t* pDestData, ComPtr<IMFSample> src) {
+  ComPtr<IMFMediaBuffer> srcBuffer;
+  HRESULT hr = src->ConvertToContiguousBuffer(&srcBuffer);
+  if (FAILED(hr)) {
+    return hr;
+  }
+
+  DWORD curLength;
+  hr = srcBuffer->GetCurrentLength(&curLength);
+  if (FAILED(hr))
+    return hr;
+
+  if (curLength > 0) {
+    BYTE* pSrcData;
+    DWORD maxLen, curLen;
+    hr = srcBuffer->Lock(&pSrcData, &maxLen, &curLen);
+    if (FAILED(hr))
+      return hr;
+
+    memcpy(pDestData, pSrcData, curLen);
+
+	hr = srcBuffer->Unlock();
+    if (FAILED(hr))
+      return hr;
+  }
+
+  return S_OK;
+}
+
+/** 
+ * Workaround [MF H264 bug: Output status is never set, even when ready]
+ *  => For now, always mark "ready".
+ */
+HRESULT GetOutputStatus(ComPtr<IMFTransform> decoder, DWORD* output_status) {
+  HRESULT hr = decoder->GetOutputStatus(output_status);
+
+  // Don't trust output status.
+  *output_status = MFT_OUTPUT_STATUS_SAMPLE_READY;
+  return hr;
+}
+
 HRESULT WinUWPH264DecoderImpl::FlushFrames(uint32_t rtp_timestamp, uint64_t ntp_time_ms) {
 
   HRESULT hr;
   DWORD outputStatus;
 
-  while (SUCCEEDED(hr = m_spDecoder->GetOutputStatus(&outputStatus)) &&
+  while (SUCCEEDED(hr = GetOutputStatus(m_spDecoder, &outputStatus)) &&
          outputStatus == MFT_OUTPUT_STATUS_SAMPLE_READY)
   {
     // Get needed size of our output buffer
@@ -227,54 +307,35 @@ HRESULT WinUWPH264DecoderImpl::FlushFrames(uint32_t rtp_timestamp, uint64_t ntp_
 	// Invoke the Media Foundation decoder
     DWORD status;
     hr = m_spDecoder->ProcessOutput(0, 1, &outputDataBuffer, &status);
+    // TODO: if we need to detect stream format changes, read
+    // MF_E_TRANSFORM_STREAM_CHANGE here and adjust
 
-	// TODO: if we need to detect stream format changes, read MF_E_TRANSFORM_STREAM_CHANGE here and adjust
-    if (status == MF_E_TRANSFORM_NEED_MORE_INPUT) {
-	  // Ideally, this won't happen (since that'd mean we allocated buffers unnecessarily as per GetOutputStatus())
-	  // but there's nothing wrong (we're just out of data), so we return OK.
-      return S_OK;
-	}
-
-	// Get the sample's buffer.
-    ComPtr<IMF2DBuffer> sp2DBuffer;
-    hr = spOutBuffer->QueryInterface(IID_IMF2DBuffer, (void**)&sp2DBuffer);
     if (FAILED(hr))
-      return hr;
-
-	// Get raw pointer to image buffer
-    BYTE* pData;
-    LONG stride;
-    hr = sp2DBuffer->Lock2D(&pData, &stride);
-    if (FAILED(hr))
-    return hr;
+      return hr; /* can return MF_E_TRANSFORM_NEED_MORE_INPUT */
 
 	// TODO: libvpx vp8 impl uses I420BufferPool to make these which should reduce allocations.
     rtc::scoped_refptr<I420Buffer> buffer(new rtc::RefCountedObject<I420Buffer>(width_, height_));
 
-	// Tiny hack. Use Y pointer to write entire frame.
-	BYTE* pI420BufRaw = buffer->MutableDataY();
-
-	// Copy data, ignoring pad. Faster than using sample->ConvertToContiguousBuffer which would
-	// result in extra copying since it doesn't write directly into the webrtc buffer like we can.
-    // TODO: use libyuv::copy if needed (if MF YUV I420 format is somehow different from WebRTC's)
-	for (DWORD row = 0; row < height_; row++) {
-      memcpy(pI420BufRaw, pData, width_);
-      pI420BufRaw += width_;
-      pData += stride;
+	// Copy raw output sample data to video frame buffer.
+    ComPtr<IMF2DBuffer> sp2DBuffer;
+    hr = spOutBuffer->QueryInterface(IID_IMF2DBuffer, (void**)&sp2DBuffer);
+    if (SUCCEEDED(hr)) {
+      hr = CopyOutBuffer2D(buffer->MutableDataY(), sp2DBuffer, width_, height_);
+    } else {
+      // No 2D buffer support.
+      hr = CopyOutBuffer(buffer->MutableDataY(), spOutSample);
 	}
 
-	// We got our data. Let it go... let it go.
-	hr = sp2DBuffer->Unlock2D();
-    if (FAILED(hr))
+	if (FAILED(hr))
       return hr;
-
+	
 	LONGLONG sampleTime;
     hr = spOutSample->GetSampleTime(&sampleTime);
     if (FAILED(hr))
       return hr;
 
 	// We use the timestamp direct from Media Foundation sample, which will be interpolated properly
-	// Note: rtp_timestamp is unused, but could be forwarded from earliest frame as we do below for ntp
+	// TODO: should we use rtp overload of this constructor? Hopefully this one can derive it from timestamp_us
     VideoFrame decodedFrame(buffer, kVideoRotation_0, sampleTime / 10 /* convert 100-nanosecond unit to microseconds */);
     
 	// Use ntp time from the earliest frame
@@ -288,23 +349,27 @@ HRESULT WinUWPH264DecoderImpl::FlushFrames(uint32_t rtp_timestamp, uint64_t ntp_
 }
 
 // TODO: handle missing frames not yet implemented
+// TODO: reuse input buffers if possible to save on allocations
+// Note: can return MF_E_NOTACCEPTING (though it shouldn't since last loop should've flushed)
 HRESULT WinUWPH264DecoderImpl::EnqueueFrame(const EncodedImage& input_image, bool missing_frames) {
-  
   // Create a MF buffer from our data
-  // TODO: reuse input buffers if possible to save on allocations
   ComPtr<IMFMediaBuffer> spBuffer;
   HRESULT hr = MFCreateMemoryBuffer(input_image._length, &spBuffer);
   if (FAILED(hr))
     return hr;
+
   DWORD maxLen, curLen;
   BYTE* pData;
   hr = spBuffer->Lock(&pData, &maxLen, &curLen);
   if (FAILED(hr))
     return hr;
+
   memcpy(pData, input_image._buffer, input_image._length);
+
   hr = spBuffer->Unlock();
   if (FAILED(hr))
     return hr;
+
   hr = spBuffer->SetCurrentLength(input_image._length);
   if (FAILED(hr))
     return hr;
@@ -314,61 +379,44 @@ HRESULT WinUWPH264DecoderImpl::EnqueueFrame(const EncodedImage& input_image, boo
   hr = MFCreateSample(&spSample);
   if (FAILED(hr))
     return hr;
+
   hr = spSample->AddBuffer(spBuffer.Get());
   if (FAILED(hr))
     return hr;
+
   hr = spSample->SetSampleTime(input_image.capture_time_ms_ * 10000 /* convert milliseconds to 100-nanosecond unit */);
   if (FAILED(hr))
     return hr;
 
   // Enqueue sample with Media Foundation
-  hr = m_spDecoder->ProcessInput(0, spSample.Get(), 0);
-  if (FAILED(hr)) {
-	// TODO: log here especially. This would mean MF couldn't accept the sample, so we will drop it.
-	// We attempt to ensure this won't happen by flushing any potentially pending output before enqueuing.
-    return hr;
-  }
-
-  return S_OK;
+  return m_spDecoder->ProcessInput(0, spSample.Get(), 0);
 }
 
 int WinUWPH264DecoderImpl::Decode(const EncodedImage& input_image,
   bool missing_frames,
   const CodecSpecificInfo* codec_specific_info,
   int64_t render_time_ms) {
+  HRESULT hr = S_OK;
 
   // TODO: add more precondition checks
   if (decodeCompleteCallback_ == NULL) {
     return WEBRTC_VIDEO_CODEC_UNINITIALIZED;
   }
 
-  DWORD inputStatus;
-  HRESULT hr = m_spDecoder->GetInputStatus(0, &inputStatus);
-
-  if (FAILED(hr)) {
-    return WEBRTC_VIDEO_CODEC_ERROR;
-  }
-
-  if (inputStatus != MFT_INPUT_STATUS_ACCEPT_DATA) {
-	// Process output until the stream can accept the new frame.
-	// Use last frame times (TODO: this may not be sufficient)
-    int res = FlushFrames(lastRtpTimestamp_, lastNtpTimeMs_);
-    if (FAILED(hr))
-      return WEBRTC_VIDEO_CODEC_ERROR;
-  }
-
-  // Save ntp in case MF has residual output data from this frame on the next one
-  lastRtpTimestamp_ = input_image.Timestamp;
-  lastNtpTimeMs_ = input_image.ntp_time_ms_;
-
   // Enqueue the new frame with Media Foundation
   hr = EnqueueFrame(input_image, missing_frames);
+  if (hr == MF_E_NOTACCEPTING) {
+    // For robustness (shouldn't happen). Flush any old MF data blocking new frames.
+    m_spDecoder->ProcessMessage(MFT_MESSAGE_COMMAND_FLUSH, NULL);
+    hr = EnqueueFrame(input_image, missing_frames);
+  }
+
   if (FAILED(hr))
     return WEBRTC_VIDEO_CODEC_ERROR;
 
-  // Flush any decoded resulting from this new frame, and invoke callback
-  hr = FlushFrames(input_image.Timestamp, input_image.ntp_time_ms_);
-  if (FAILED(hr))
+  // Flush any decoded samples resulting from new frame, invoking callback
+  hr = FlushFrames(input_image.Timestamp(), input_image.ntp_time_ms_);
+  if (FAILED(hr) && hr != MF_E_TRANSFORM_NEED_MORE_INPUT)
     return WEBRTC_VIDEO_CODEC_ERROR;
   
   return WEBRTC_VIDEO_CODEC_OK;
@@ -386,13 +434,16 @@ int WinUWPH264DecoderImpl::Release() {
   OutputDebugString(L"WinUWPH264DecoderImpl::Release()\n");
   HRESULT hr = S_OK;
 
-  if (SUCCEEDED(hr))
-    hr = m_spDecoder->ProcessMessage(MFT_MESSAGE_NOTIFY_END_OF_STREAM, 0);
-  if (SUCCEEDED(hr))
-    hr = m_spDecoder->ProcessMessage(MFT_MESSAGE_COMMAND_DRAIN, NULL);
-  if (SUCCEEDED(hr))
-    hr = m_spDecoder->ProcessMessage(MFT_MESSAGE_COMMAND_FLUSH, NULL);
+  if (m_spDecoder != NULL) {
+    if (SUCCEEDED(hr))
+      hr = m_spDecoder->ProcessMessage(MFT_MESSAGE_NOTIFY_END_OF_STREAM, 0);
+    if (SUCCEEDED(hr))
+      hr = m_spDecoder->ProcessMessage(MFT_MESSAGE_COMMAND_DRAIN, NULL);
+    if (SUCCEEDED(hr))
+      hr = m_spDecoder->ProcessMessage(MFT_MESSAGE_COMMAND_FLUSH, NULL);
+  }
 
+  // TODO: this should only be called if CoInitializeEx succeeded to begin with since these must be balanced.
   if (SUCCEEDED(hr))
     CoUninitialize();
 
